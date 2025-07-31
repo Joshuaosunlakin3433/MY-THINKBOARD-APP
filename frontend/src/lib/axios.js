12e931357cff9ceafaa37a00a4728bde039502a1
Joshuaosunlakin3433 <64253576+Joshuaosunlakin3433@users.noreply.github.com>
2025-07-31 12:52:18 +0100

    prepared for development

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+//in production there's nothing called local host
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api":"/api/"
 export const api = axios.create({
-    baseURL:"http://localhost:5001/api"
+    baseURL: BASE_URL
 })

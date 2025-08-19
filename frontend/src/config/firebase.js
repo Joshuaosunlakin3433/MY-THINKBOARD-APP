b72f46e917ab43d62d4112f23abfda098ceeb79e
Joshuaosunlakin3433 <64253576+Joshuaosunlakin3433@users.noreply.github.com>
2025-08-19 21:35:39 +0100

    Add Firebase configuration with project credentials

---
+++ frontend/src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/App";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAc1aEXdyv4j7Q2Zd46bwd-M8LPriwb-8I",
+  authDomain: "thinkboard-auth.firebaseapp.com",
+  projectId: "thinkboard-auth",
+  storageBucket: "thinkboard-auth.firebasestorage.app",
+  messagingSenderId: "786235743780",
+  appId: "1:786235743780:web:61b01fb9d94bdf2225c2b1"
+};
+
+const app = initializeApp(firebaseConfig) //Firebase initialization
+
+export const auth = getAuth(app) //Authentication-specific function
